b1d18c04c5775f74613647c81ecd3711a62d3850
GordonZhang2024 <jp20171211@163.com>
2024-03-22 18:55:56 +0800

    tkMarker

---
+++ src/converter.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+import re
+
+def convert(markdown: str):
+    '''
+    function convert(): convert Markdown to HTML
+    '''
+    markdown = markdown.splitlines()
+    html = ''    
+    for index, line in enumerate(markdown):
+        head = re.match(r'#+\s', line)
+        if head:
+            head = str(head.group(0))
+            lenth = len(head) - 1
+            if lenth <= 6:
+                line = line.replace(head, f'<h{lenth}>')
+                line = line + f'</h{lenth}><hr/>'
+
+        bold = re.findall(r'[\*_]{2}[\w\s]+?[\*_]{2}', line)
+        if bold:
+            for i in bold:
+                strong = '<strong>' + i[2:-2] + '</strong>'
+                line = line.replace(i, strong)
+
+        italic = re.findall(r'[\*_][\w\s</>]+?[\*_]', line)
+        if italic:
+            for i in italic:
+                em = '<em>' + i[1:-1] + '</em>'
+                line = line.replace(i, em)
+
+        quote = re.match(r'[>\s]+\s', line)
+        if quote:
+            quote = str(quote.group(0))
+            lenth = len(quote) - 1
+            line = line.replace(quote, lenth * '<blockquote>')
+            line = line + lenth * '</blockquote>'
+
+        code = re.findall(r'`[\w\s</>]+?`', line)
+        if code:
+            for i in code:
+                c = '<q><code>' + i[1:-1] + '</code></q>'
+                line = line.replace(i, c)
+
+        strikethrough = re.findall(r'~{2}[\w\s]+?~{2}', line)
+        if strikethrough:
+            for i in strikethrough:
+                s = '<s>' + i[2:-2] + '</s>'
+                line = line.replace(i, s)
+
+        link = re.match(r'\[[\w\s]+?\]\([\w\s]+?\)', line)
+        if link:
+            link = str(link.group(0))
+            text = re.match(r'\[[\w\s]+?\]', link).group(0)
+            text = text.replace('(', '')
+            text = text.replace(')', '')
+            href = re.match(r'\([\w\s]+?\)', link).group(0)
+            href = href.replace('(', '')
+            href = href.replace(')', '')
+            l = f'<a href={href}>{text}</a>'
+            line = line.replace(link, l)
+
+        img = re.match(r'!\[\]\([\w\W]+?\)', line)
+        if img:
+            img = str(img.group(0))
+            src = img.replace('![]', '')\
+                     .replace('(', '')\
+                     .replace(')', '')
+            image = f'<img src={src}/>'
+            line = line.replace(img, image)
+        
+        line = line + '<br/>'
+        html = html + line
+    html = '''\
+<html>
+    <style>
+    p {
+        background: lightgrey;
+    }
+    q {
+        background: lightgrey;
+     }
+    </style>
+</html>
+'''\
+    + html\
+    + '</html>'
+    html = html.replace('<script>', '')\
+               .replace('</script>', '')
+    
+    quoted_code  = re.findall(r'```[\w\W]+?```', html)
+    if quoted_code:
+        for i in quoted_code:
+            quoted_code = '<code>' + i[3:-3] + '</code>'
+            html = html.replace(i, quoted_code)
+
+    return html
---
+++ src/editor.py
@@ -0,0 +1,49 @@
+import tkinter
+import webbrowser
+import os
+from tkinter import filedialog, scrolledtext
+from converter import convert
+
+def load_preview():
+    markdown_text_for_preview = text.get('1.0', 'end')
+    preview_file = f'{home}/.tkmarker_preview'
+    html = convert(markdown_text_for_preview)
+    with open(preview_file, 'w') as p:
+        p.write(html)
+
+    webbrowser.open(preview_file)
+
+def save():
+    global file_name
+    if file_name == 'New File':
+        file_name = filedialog.asksaveasfilename(initialdir='~/Documents')
+        editor.title(file_name)
+        
+    markdown_text = text.get('1.0', 'end')
+    with open(file_name, mode='w+') as f:
+        f.write(markdown_text)
+        
+def new_file():
+    global file_name
+    global editor
+    editor = tkinter.Tk()
+    editor.title(file_name)
+    menubar = tkinter.Menu(editor)
+    file = tkinter.Menu(menubar, tearoff=0)
+    menubar.add_cascade(label='File', menu=file)
+    menubar.add_command(label='Preview', command=load_preview)
+    
+    global text
+    text = scrolledtext.ScrolledText(editor)
+    text.grid()
+
+    file.add_command(label='New File')
+    #file.add_command(label='Open...')
+    #file.add_command(label='Save', command=save)
+
+    editor.config(menu=menubar)
+    editor.mainloop()
+
+home = os.path.expanduser('~')
+file_name = 'New File'
+new_file()
