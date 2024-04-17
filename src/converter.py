5613822c65e1d3dd029fc0aa7e3763b1c7b7c078
Gordon Zhang <jp20171211@163.com>
2024-04-17 18:38:32 +0800

    Update converter.py

--- src/converter.py
+++ src/converter.py
@@ -5,6 +5,25 @@ Module converter
 Convert Markdown to HTML
 """
 
+"""
+tkMarker
+    A Markdown editor using tkinter
+Copyright (C) 2024 Gordon Zhang
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import re
 
 def convert(markdown: str, preview=False, file_path='./') -> str:
