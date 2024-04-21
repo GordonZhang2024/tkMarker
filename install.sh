574fb10246ae79086932e919707dc9ef95218e85
GordonZhang2024 <jp20171211@163.com>
2024-04-21 12:51:11 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,13 +19,7 @@
 
 tkMarker_dir=$(pwd)
 src_dir="${tkMarker_dir}/src/"
-dist_dir="${tkMarker_dir}/dist/main/"
 
-#desktop_icon="
-#[Desktop Entry]\nTerminal=false\nExec=$tkMarker_dir/dist/main/main\nName=tkMarker\nType=Application\nIcon=$tkMarker_dir/icon.png\n
-#"
-#sudo echo -e $desktop_icon > /usr/share/applications/tkmarker.desktop
-
-sudo ln -s "${dist_dir}/main" /usr/bin/tkmarker
+sudo ln -s "${src_dir}editor.py" /usr/bin/tkmarker
 cd ~
 mkdir .tkmarker/
