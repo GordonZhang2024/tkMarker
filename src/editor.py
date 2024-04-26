f3f12286413561548672cafbc9b484a59efb75e4
GordonZhang2024 <jp20171211@163.com>
2024-04-26 15:34:20 +0800

    Update converter.py

--- src/editor.py
+++ src/editor.py
@@ -38,7 +38,7 @@ filetypes = [('Markdown', '*.md'), ("All Files", "*.*")]
 
 
 def load_preview():
-    """Load the preview"""
+    # Load the preview
     markdown_text_for_preview = text.get('1.0', 'end')  # Get the text
     preview_file = f'{home}/.tkmarker/{file_uuid}'
     html = convert(markdown_text_for_preview, preview=True,
@@ -51,7 +51,7 @@ def load_preview():
 
 
 def refresh_preview(arg):
-    """Refresh the preview"""
+    # Refresh the preview
     markdown_text_for_preview = text.get('1.0', 'end')
     preview_file = f'{home}/.tkmarker/{file_uuid}'
     html = convert(markdown_text_for_preview, preview=True,
@@ -61,7 +61,7 @@ def refresh_preview(arg):
 
 
 def convert_to_html():
-    """Convert Markdown document to HTML"""
+    # Convert Markdown document to HTML
     markdown_text = text.get('1.0', 'end')
     output_file = filedialog.asksaveasfilename(filetypes=[('HTML', '*.html')])
     html = convert(markdown_text)
@@ -70,27 +70,27 @@ def convert_to_html():
 
 
 def show_popup_menu(event):
-    """Show the popup menu"""
+    # Show the popup menu
     edit.post(event.x_root, event.y_root)
 
 
 def paste():
-    """Paste"""
+    # Paste
     text.event_generate('<<Paste>>')
 
 
 def copy():
-    """Copy"""
+    # Copy
     text.event_generate('<<Copy>>')
 
 
 def cut():
-    """Cut"""
+    # Cut
     text.event_generate('<<Cut>>')
 
 
 def open_file():
-    """Open a file"""
+    # pen a file
     global filename
     filename = filedialog.askopenfilename(
         initialdir='~/Documents', filetypes=filetypes)
@@ -102,7 +102,7 @@ def open_file():
 
 
 def save():
-    """Save"""
+    # Save
     global filename
     if filename == 'New File':
         filename = filedialog.asksaveasfilename(
@@ -113,14 +113,15 @@ def save():
     with open(filename, 'w+', encoding='utf-8') as f:
         f.write(markdown_text)
 
+
 def new_file():
-    """Open new file"""
+    # pen new file
     global filename
     global editor
     editor = tkinter.Tk()
     editor.title(filename)
 
-    #Set full screen
+    # Set full screen
     width = editor.winfo_screenwidth()
     height = editor.winfo_screenheight()
     editor.geometry(f'{width}x{height}')
