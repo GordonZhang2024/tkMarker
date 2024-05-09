f28e56e4be7a3515013af218007e5f786c7c3c21
Gordon Zhang <jp20171211@163.com>
2024-05-09 15:16:19 +0800

    Update get_help.py

--- src/get_help.py
+++ src/get_help.py
@@ -13,19 +13,6 @@ import webbrowser
 tkMarker
     A Markdown editor using tkinter
 Copyright (C) 2024 Gordon Zhang
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 REPOS_URL = 'https://github.com/GordonZhang2024/tkMarker'
