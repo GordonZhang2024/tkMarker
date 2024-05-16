05a56a68bfff113d7c159f95245ccf83c17ed248
GordonZhang2024 <jp20171211@163.com>
2024-05-16 11:42:08 +0000

    Update the installer

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@
 # Copyright (C) 2024 Gordon Zhang
 
 
+# Project info
 echo -e "\x1b[1mtkMarker |  A Markdown editor\x1b[0m"
 echo    "Copyright (C) 2024 Gordon Zhang"
 echo    "Website: https://gordonzhang.pythonanywhere.com/projects/tkMarker/"
@@ -13,9 +14,11 @@ echo
 
 echo -e "\x1b[36mInstalling tkMarker\x1b[0m ..."
 
+# Test if the user has already installed tkmarker.
 if test -f ~/.local/bin/tkmarker; then
     echo "It seems like you have already installed tkmarker."
 else
+    # Test if the user has alerady installed Python.
     if test -f /usr/bin/python -o -f /bin/python -o -f ~/.local/bin/python; then
         py_version=$(python --version)
         echo -e "\x1b[1mMake sure you have already installed Python >= 3.8 and tkinter!\x1b[0m"
@@ -23,9 +26,11 @@ else
         tkMarker_dir=$(pwd)
         src_dir="${tkMarker_dir}/src/"
 
+        # Create a cymbolic link at ~/.local/bin/
         echo "Creating symbolic link (~/.local/bin/tkmarker) ..."
         ln -s "${src_dir}editor.py" ~/.local/bin/tkmarker
 
+        # Create .tkMarker/ directory (It's for the preview files.)
         echo "Creating .tkMarker directory ..."
         cd ~
         mkdir .tkmarker/
